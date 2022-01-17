34a2de2224c37d8acb8ba6932243d36ee7d52b04
Ritche Reluao <34097542+ritchereluao@users.noreply.github.com>
2022-01-17 14:58:44 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request
 import requests
 import smtplib
 
-blog_url = "BLOG URL"
+blog_url = "BLOG_ENDPOINT"
 posts = requests.get(url=blog_url).json()
 
 app = Flask(__name__)
@@ -22,18 +22,19 @@ def about():
 def contact():
     if request.method == "POST":
         data = request.form
-        with smtplib.SMTP("smtp.gmail.com", 587) as connection:
-            connection.starttls()
-            connection.login(user="EMAIL", password="PASSWORD")
-            connection.sendmail(
-                from_addr="EMAIL",
-                to_addrs="EMAIL",
-                msg=f"Subject: New Message from Blog!\n\nName: {data['name']}"
-                    f"\nEmail: {data['email']}\nPhone: {data['phone']}\nMessage:\n\t{data['message']}")
-
+        send_email(data["name"], data["email"], data["phone"], data["message"])
         return render_template("contact.html", msg_sent=True)
     else:
         return render_template("contact.html", msg_sent=False)
+
+
+def send_email(name, email, phone, message):
+    email_message = f"Subject: New Message from Blog!\n\n" \
+                    f"Name: {name}\nEmail: {email}\nPhone: {phone}\nMessage:\n\t{message}"
+    with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+        connection.starttls()
+        connection.login(user="EMAIL@gmail.com", password="EMAIL")
+        connection.sendmail(from_addr="EMAIL@gmail.com", to_addrs="EMAIL@gmail.com", msg=email_message)
 
 
 @app.route("/post/<int:index>")
